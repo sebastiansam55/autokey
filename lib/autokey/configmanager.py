f088b6e5c2d372e0c64fd3ff0cf6c71f3556ba6a
Sam <sebastiansam55@gmail.com>
2020-09-28 07:51:29 -0400

    Add custom sort for popup menu (gtk GUI changes only)

--- lib/autokey/configmanager.py
+++ lib/autokey/configmanager.py
@@ -45,6 +45,7 @@ SERVICE_RUNNING = "serviceRunning"
 MENU_TAKES_FOCUS = "menuTakesFocus"
 SHOW_TRAY_ICON = "showTrayIcon"
 SORT_BY_USAGE_COUNT = "sortByUsageCount"
+SORT_BY_CUSTOM = "customSortForHotkey"
 #DETECT_UNWANTED_ABBR = "detectUnwanted"
 PROMPT_TO_SAVE = "promptToSave"
 #PREDICTIVE_LENGTH = "predictiveLength"
@@ -363,6 +364,7 @@ class ConfigManager:
                 MENU_TAKES_FOCUS: False,
                 SHOW_TRAY_ICON: True,
                 SORT_BY_USAGE_COUNT: True,
+                SORT_BY_CUSTOM: False,
                 #DETECT_UNWANTED_ABBR: False,
                 PROMPT_TO_SAVE:False,
                 #PREDICTIVE_LENGTH: 5,
